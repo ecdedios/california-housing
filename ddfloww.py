5e1cad3f4162568ad46f02d440649d070cefb193
Ednalyn C. De Dios <ednalyn.dedios@gmail.com>
2019-05-02 20:34:56 -0500

    initial commit

---
+++ ddfloww.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+"""
+This script contains code used by the following jupytr notebooks:
+
+1.
+2.
+3.
+
+"""
+
+
+
+# ===========
+# ENVIRONMENT
+# ===========
+
+
+
+
+
+# ===========
+# ACQUISITION
+# ===========
+
+
+
+
+
+# ===========
+# PREPARATION
+# ===========
+
+
+
+
+
+
+
+# ========
+# MODELING
+# ========
+
+
+
+
+
+
+
+
+# ==================================================
+# MAIN
+# ==================================================
+
+
+def clear():
+	os.system("cls" if os.name == "nt" else "clear")
+
+def main():
+    """Main entry point for the script."""
+    pass
+
+
+if __name__ == '__main__':
+    sys.exit(main())
+
+
+
+
+
+
+
+
+
+
+__author__ = "Ednalyn C. De Dios, et al."
+__copyright__ = "Copyright 2019"
+__credits__ = [" "]
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Ednalyn C. De Dios"
+__email__ = "ednalyn.dedios@gmail.com"
+__status__ = "Prototype"
